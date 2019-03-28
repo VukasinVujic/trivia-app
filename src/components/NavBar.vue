14525148a36b40e38b396e6f6ff801fbe2837d8d
VukasinVujic <vukasinvujic021@gmail.com>
2019-03-28 18:43:38 +0100

    Zadatak 2

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,19 +1,6 @@
 <template>
   <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-    <a class="navbar-brand" href="#">Navbar</a>
-    <div class="collapse navbar-collapse" id="navbarNav">
-      <ul class="navbar-nav">
-        <li class="nav-item">
-          <a class="nav-link" href="#">Features</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#">Pricing</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
-        </li>
-      </ul>
-    </div>
+    <router-link :to="{name:'trivia'}" class="navbar-brand">Trivia app</router-link>
   </nav>
 </template>
 
--- src/main.js
+++ src/main.js
@@ -6,8 +6,10 @@ import { router } from "./router";
 import BootstrapVue from "bootstrap-vue";
 import "bootstrap/dist/css/bootstrap.css";
 import "bootstrap-vue/dist/bootstrap-vue.css";
-
 Vue.use(BootstrapVue);
+
+import Vuex from "vuex";
+Vue.use(Vuex);
 
 Vue.config.productionTip = false;
 
